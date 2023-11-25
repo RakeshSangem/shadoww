95adc6a409f21dd6d7e8efbd69b7fb2ea63320a4
Rakesh Sangem <rakeshsangem88@gmail.com>
2023-11-25 11:56:20 +0530

    Add meta tags for SEO and social media sharing

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -13,6 +13,24 @@ export default function Layout({ children }: LayoutProps): JSX.Element {
         <title>Box Shadow Generator by Rakesh Sangem</title>
         <meta name="description" content="Generate box shadow css" />
         <link rel="icon" href="/favicon.ico" />
+        <meta name="author" content="Rakesh Sangem" />
+        <meta
+          name="keywords"
+          content="box shadow, css, generator, rakesh sangem"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <meta
+          property="og:title"
+          content="Box Shadow Generator by Rakesh Sangem"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://box-shadow.vercel.app/" />
+        <meta property="og:description" content="Generate box shadow css" />
+        <meta
+          property="og:image"
+          content="https://box-shadow.vercel.app/og-image.png"
+        />
       </Head>
 
       <Navbar />
